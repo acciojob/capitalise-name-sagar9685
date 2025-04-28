56abbe4aa186aa103e7bce833a1a8a46152f9a2b
sagar9685 <160707983+sagar9685@users.noreply.github.com>
2025-04-28 14:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var inputField = document.getElementByID('fname');
+inputField.addEventListner('blur',function(){
+	inputField.value = inputField.value.toUpperCase();
+})
+	
